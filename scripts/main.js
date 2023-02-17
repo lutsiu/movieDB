9a2515ad2f1f40f9b19111f916942cbc827f13bc
Oleksandr Lutsiuk <oleksandr.lutsiuk.1603@gmail.com>
2023-02-17 10:02:26 +0100

    hide the api

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1a00a39f9amsh57b63c28cfed523p1d62bejsn295f760a437d',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
-};
-
-const fun = async (movie) => {
-  const res = await fetch('https://imdb8.p.rapidapi.com/auto-complete?q=breakingbad', options);
+import {key, url} from './config.js';
+const getGenre = async (movie) => {
+  /* const res = await fetch(`${url}/movie/top_rated?api_key=${key}&genre=western`); // &page=integer */ // top rated
+  const res = await fetch(`${url}/genre/movie/list?api_key=${key}`); // &page=integer
+  /* const res = await fetch(`
+  ${url}/search/movie?api_key=${key}&language=en-US&query=godfather&page=1&include_adult=false`) */ // query to search film by title
   const data = await res.json();
-  console.log(data);
+  
+  console.log(data.genres);
+/*   console.log(data); */
+  const a = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=14&page=2`); // get films by genres
+  const b = await a.json();
+  console.log(b);
 };  
-/* fun(); */
+getGenre();
